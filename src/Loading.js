beb7045ece225c1976db8c3f041e3f48529dd06b
Bernard Gaitho <gaithoben@gmail.com>
2018-01-13 21:07:55 +0300

    feat(compile): Export all components
    This release exports all components for use

--- src/Loading.js
+++ src/Loading.js
@@ -8,3 +8,5 @@ export const Loading = () => (
     <CircularProgress className="loading-icon-mui" />
   </div>
 );
+
+export default Loading;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,55 @@
-import StaticListSelector from './StaticListSelector';
-import MultiStaticListSelector from './MultiStaticListSelector';
-import SimpleDataGrid from './SimpleDataGrid';
-import PhoneInput from './PhoneInput';
-import SimpleQuill from './SimpleQuill';
-import MediumQuill from './MediumQuill';
 import CheckBox from './CheckBox';
-import ImagesUpload from './ImagesUpload';
+import AddToCartButton from './AddToCartButton';
 import CountrySelector from './CountrySelector';
+import DataGridWithDetailView from './DataGridWithDetailView';
+import DatePicker from './DatePicker';
+import FilesUpload from './FilesUpload';
+import FullDataGrid from './FullDataGrid';
+import ImagesUpload from './ImagesUpload';
+import Loading from './Loading';
+import MediumQuill from './MediumQuill';
+import MultiRemoteSelector from './MultiRemoteSelector';
+import MultiSelector from './MultiSelector';
+import MultiStaticListSelector from './MultiStaticListSelector';
+import PhoneInput from './PhoneInput';
+import Progress from './Progress';
+import RemoteDataGrid from './RemoteDataGrid';
+import RemoteDataGridWithDetailView from './RemoteDataGridWithDetailView';
+import RemoteSelector from './RemoteSelector';
+import SearchInput from './SearchInput';
+import Selector from './Selector';
+import SimpleDataGrid from './SimpleDataGrid';
+import SimpleQuill from './SimpleQuill';
+import StaticListSelector from './StaticListSelector';
+import TableHeaderBar from './TableHeaderBar';
+import Upload from './Upload';
+import UserRoles from './UserRoles';
 
 export {
-  StaticListSelector,
-  MultiStaticListSelector,
-  SimpleDataGrid,
-  PhoneInput,
-  SimpleQuill,
   CheckBox,
-  MediumQuill,
-  ImagesUpload,
+  AddToCartButton,
   CountrySelector,
+  DataGridWithDetailView,
+  DatePicker,
+  FilesUpload,
+  FullDataGrid,
+  ImagesUpload,
+  Loading,
+  MediumQuill,
+  MultiRemoteSelector,
+  MultiSelector,
+  MultiStaticListSelector,
+  PhoneInput,
+  Progress,
+  RemoteDataGrid,
+  RemoteDataGridWithDetailView,
+  RemoteSelector,
+  SearchInput,
+  Selector,
+  SimpleDataGrid,
+  SimpleQuill,
+  StaticListSelector,
+  TableHeaderBar,
+  Upload,
+  UserRoles
 };
