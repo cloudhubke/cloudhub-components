db95ba3c30044823619dd613216554090160abb8
Bernard Gaitho <gaithoben@gmail.com>
2018-07-03 16:30:21 +0300

    fix(Loading): Modified the Loading component
    Loading component now uses material ui

--- src/Loading.js
+++ src/Loading.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-import './loading.css';
-
-export const Loading = () => (
-  <div className="loading-shading-mui">
-    <CircularProgress className="loading-icon-mui" />
-  </div>
-);
-
-export default Loading;
+export default function Loading(props) {
+  if (props.isLoading) {
+    if (props.timedOut) {
+      return <div>Loader timed out!</div>;
+    } else if (props.pastDelay) {
+      return (
+        <div
+          style={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <CircularProgress color="secondary" size={50} />
+        </div>
+      );
+    }
+    return null;
+  } else if (props.error) {
+    return <div>Error! Component failed to load</div>;
+  }
+  return null;
+}
