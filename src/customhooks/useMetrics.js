977c6239cc93a0ca4d4d3a168a55b839a2c47c58
Bernard Gaitho <gaithoben@gmail.com>
2019-11-07 08:11:45 +0300

    fix(useMetrics): isMobile
    add isMobile variable

--- src/customhooks/useMetrics.js
+++ src/customhooks/useMetrics.js
@@ -2,17 +2,20 @@ import React, { useEffect } from 'react';
 
 const hasWindow = typeof window !== 'undefined';
 
-let isMobile = false;
-
-if (global.navigator) {
-  isMobile = navigator.userAgent.match(
-    /(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i
-  );
-}
-
 const useMetrics = () => {
   const [width, setWidth] = React.useState(hasWindow && window.innerWidth);
   const [height, setHeight] = React.useState(hasWindow && window.innerHeight);
+
+  const [isMobile, setIsMobile] = React.useState(false);
+
+  React.useEffect(() => {
+    if (global.navigator) {
+      const isMobile = navigator.userAgent.match(
+        /(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i
+      );
+      setIsMobile(isMobile);
+    }
+  }, []);
 
   useEffect(() => {
     const resize = () => {
