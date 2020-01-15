cd46a37e1da992d5ac714a0e767ec3e4006e97df
Bernard Gaitho <gaithoben@gmail.com>
2020-01-16 02:54:54 +0300

    fix(hooks): add hook
    added useEventListener hook

--- src/customhooks/index.js
+++ src/customhooks/index.js
@@ -3,5 +3,6 @@ export { default as useDebounce } from './useDebounce';
 export { default as useInterval } from './useInterval';
 export { default as useMetrics } from './useMetrics';
 export { default as useLocation } from './useLocation';
+export { default as useEventListener } from './useEventListener';
 export { useWindowSize } from '@reach/window-size';
 export { useRect } from '@reach/rect';
