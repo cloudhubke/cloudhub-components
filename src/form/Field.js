72d258846e00e40272f596e31d9fbf737b4ad380
Bernard Gaitho <gaithoben@gmail.com>
2020-02-29 18:50:55 +0300

    fix(Fix): FIx email and required
    fix email field error

--- src/form/Field.js
+++ src/form/Field.js
@@ -47,7 +47,7 @@ const maxFieldLength = max => value => {
 const validateEmail = value => {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const valid = re.test(value);
-  if (!valid) {
+  if (value && !valid) {
     return `Should be a valid email address`;
   }
   return undefined;
