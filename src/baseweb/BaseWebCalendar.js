8fc44e03bcc5d25362014cc648ed254cfc29ecb4
Bernard Gaitho <gaithoben@gmail.com>
2020-06-15 14:22:49 +0300

    fix(Baseweb): add basewebcomponents
    Added base web components in folder dist/basebweb

--- src/baseweb/BaseWebCalendar.js
+++ src/baseweb/BaseWebCalendar.js
@@ -1,11 +1,35 @@
 import * as React from 'react';
 import { StatefulCalendar } from 'baseui/datepicker';
 
-const BaseWebCalendar = () => (
-  <StatefulCalendar
-    // use the 'onChange' prop to pull data from the component into your application state
-    onChange={({ date }) => console.log(date.getTime())}
-  />
-);
+const BaseWebCalendar = ({ value, input, onChange }) => {
+  const val = value || input.value || new Date().getTime();
+
+  const [date, setDate] = React.useState([new Date(val)]);
+
+  const onChangeDate = ({ date }) => {
+    if (typeof onChange === 'function') {
+      onChange(date.getTime());
+    }
+    if (typeof input.onChange === 'function') {
+      input.onChange(date.getTime());
+    }
+    setDate(Array.isArray(date) ? date : [date]);
+  };
+
+  return (
+    <StatefulCalendar
+      // use the 'onChange' prop to pull data from the component into your application state
+      value={date}
+      onChange={onChangeDate}
+    />
+  );
+};
+BaseWebCalendar.defaultProps = {
+  onClose: () => null,
+  onChange: () => null,
+  input: {
+    onChange: () => null,
+  },
+};
 
 export default BaseWebCalendar;
