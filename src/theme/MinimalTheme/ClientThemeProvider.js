c124385907289447184cbfceed04c2123d1548f7
Bernard Gaitho <gaithoben@gmail.com>
2021-04-10 01:35:12 +0300

    fix(baseui): Lazy load base ui
    Lazy load base web ui theme

--- src/theme/MinimalTheme/ClientThemeProvider.js
+++ src/theme/MinimalTheme/ClientThemeProvider.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { BaseProvider } from 'baseui';
-import { Provider as StyletronProvider } from 'styletron-react';
-import { Client as Styletron } from 'styletron-engine-atomic';
+import Loadable from '@react-loadable/revised';
+import Box from '@material-ui/core/Box';
 import MuiThemeProvider from './core';
 
 import ThemeContext from '../ThemeContext';
+
+import Loading from '../../Loading';
+
 import localsizes from '../Sizes';
 import localcolors from '../Colors';
 import localfonts from '../Fonts';
 import { ToastContainer } from '../../toastr';
 
-import customBaseuiTheme from '../basetheme/BaseWebTheme';
-
-const engine = new Styletron({
-  hydrate: document.getElementsByClassName('_styletron_hydrate_'),
+const BaseTheme = Loadable({
+  loader: () =>
+    import(/* webpackChunkName: "BaseTheme" */ '../basetheme/BaseTheme'),
+  loading: () => (
+    <Box justifyContent="center" alignItems="center">
+      <Loading />
+    </Box>
+  ),
 });
 
 const ThemeProvider = ({
@@ -32,41 +38,22 @@ const ThemeProvider = ({
   return (
     // Apply theme for designs (Premium themes)
     <MuiThemeProvider fonts={newfonts} colors={newcolors} sizes={newsizes}>
-      <StyletronProvider value={engine}>
-        <BaseProvider
-          overrides={{
-            AppContainer: {
-              style: {
-                height: '100%',
-                flex: 1,
-                display: 'flex',
-                flexDirection: 'column',
-              },
-            },
-          }}
-          theme={customBaseuiTheme({
-            baseuiTheme,
-            colors: newcolors,
+      <BaseTheme fonts={newfonts} colors={newcolors} sizes={newsizes}>
+        <ThemeContext.Provider
+          value={{
             fonts: newfonts,
+            colors: newcolors,
             sizes: newsizes,
-          })}
+            CONFIG: props.CONFIG || {},
+            ...props,
+          }}
         >
-          <ThemeContext.Provider
-            value={{
-              fonts: newfonts,
-              colors: newcolors,
-              sizes: newsizes,
-              CONFIG: props.CONFIG || {},
-              ...props,
-            }}
-          >
-            {children}
-            <div style={{ flex: 0, zIndex: 9999 }}>
-              <ToastContainer />
-            </div>
-          </ThemeContext.Provider>
-        </BaseProvider>
-      </StyletronProvider>
+          {children}
+          <div style={{ flex: 0, zIndex: 9999 }}>
+            <ToastContainer />
+          </div>
+        </ThemeContext.Provider>
+      </BaseTheme>
     </MuiThemeProvider>
   );
 };
