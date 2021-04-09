1f6d2fdcd68dffed405b5776e27001de1c7a60a3
Bernard Gaitho <gaithoben@gmail.com>
2021-04-09 21:05:18 +0300

    fix(Bundle): Reduce bundle size
    Reduce bundle size export

--- src/theme/MinimalTheme/index.js
+++ src/theme/MinimalTheme/index.js
@@ -4,7 +4,6 @@ export const ThemeProvider = ClientThemeProvider;
 
 export { default as BadgeStatus } from './components/BadgeStatus';
 export { default as Block } from './components/Block';
-export { default as CodeSnippets } from './components/CodeSnippets';
 export { default as CopyClipboard } from './components/CopyClipboard';
 export { default as EmojiPicker } from './components/EmojiPicker';
 export { default as EmptyContent } from './components/EmptyContent';
@@ -16,45 +15,10 @@ export { default as ModalLightBox } from './components/BadgeStatus';
 export { default as MoreButton } from './components/MoreButton';
 export { default as Page } from './components/Page';
 export { default as PopoverMenu } from './components/PopoverMenu';
-export { default as ReadMdFile } from './components/ReadMdFile';
 export { default as Scrollbars } from './components/Scrollbars';
 export { default as SearchNotFound } from './components/SearchNotFound';
 
-export { default as UploadMultiFile } from './components/Upload/UploadMultiFile';
-export { default as UploadSingleFile } from './components/Upload/UploadSingleFile';
-export { default as UploadAvatar } from './components/Upload/UploadAvatar';
-
 export * from './@material-extend';
-
-// Color Utility
-export { default as PreviewColor } from './components/ColorUtility/PreviewColor';
-export { default as PickerManyColor } from './components/ColorUtility/PickerManyColor';
-export { default as PickerSingleColor } from './components/ColorUtility/PickerSingleColor';
-
-// Re-Charts
-export { default as XAxisRecharts } from './components/Charts/Recharts/XAxisRecharts';
-export { default as YAxisRecharts } from './components/Charts/Recharts/YAxisRecharts';
-export { default as LegendRecharts } from './components/Charts/Recharts/LegendRecharts';
-export { default as TooltipRecharts } from './components/Charts/Recharts/TooltipRecharts';
-export { default as CartesianGridRecharts } from './components/Charts/Recharts/CartesianGridRecharts';
-
-export { default as ApexChartsOption } from './components/Charts/Apexcharts/ApexChartsOption';
-
-// Couresols
-export { default as CarouselCustomPaging1 } from './components/Carousel/CarouselCustomPaging1';
-export { default as CarouselCustomPaging2 } from './components/Carousel/CarouselCustomPaging2';
-
-export { default as CarouselArrowsBasic1 } from './components/Carousel/CarouselArrowsBasic1';
-export { default as CarouselArrowsBasic2 } from './components/Carousel/CarouselArrowsBasic2';
-export { default as CarouselArrowsIndex } from './components/Carousel/CarouselArrowsIndex';
-
-// Animate
-export * from './components/Animate/variants';
-
-export { default as MotionInView } from './components/Animate/MotionInView';
-export { default as MotionContainer } from './components/Animate/MotionContainer';
-export { default as DialogAnimate } from './components/Animate/DialogAnimate';
-export { default as ButtonAnimate } from './components/Animate/ButtonAnimate';
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Widgets
