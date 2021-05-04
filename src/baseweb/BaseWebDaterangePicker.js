448939bb7f25e9d566f260faa6137469b9cd1f6c
Bernard Gaitho <gaithoben@gmail.com>
2021-05-04 22:40:02 +0300

    fix(Dates): update DatePicker
    Update datePickers add placeholders

--- src/baseweb/BaseWebDaterangePicker.js
+++ src/baseweb/BaseWebDaterangePicker.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import BaseWebDatePicker from './BaseWebDatePicker';
 
-const BaseWebDateRangePicker = (props) => {
-  return <BaseWebDatePicker range {...props} />;
-};
+const BaseWebDateRangePicker = (props) => (
+  <BaseWebDatePicker
+    range
+    quickSelect
+    placeholder="DD/MM/YYYY – DD/MM/YYYY"
+    {...props}
+  />
+);
 
 export default BaseWebDateRangePicker;
