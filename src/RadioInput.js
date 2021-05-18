05deb75eff9f40dd6560b32f3d9a31bc17c0f8de
John Gaitho <gaithoj@gmail.com>
2021-05-18 20:33:52 +0300

    show radio input errors

--- src/RadioInput.js
+++ src/RadioInput.js
@@ -3,9 +3,11 @@ import {
   FormControl,
   RadioGroup,
   FormControlLabel,
-  FormHelperText,
+  // FormHelperText,
   Radio,
 } from '@material-ui/core';
+import Text from './Text';
+import ThemeContext from './theme/ThemeContext';
 
 const RadioInput = ({
   input,
@@ -18,32 +20,37 @@ const RadioInput = ({
   labelKey,
   meta,
   showError,
-}) => (
-  <FormControl component="fieldset">
-    <RadioGroup
-      aria-label="radio-group"
-      name={input.name || name || 'radio-group'}
-      row={Boolean(Row)}
-      onChange={(e) => {
-        onChange(e.target.value);
-        input.onChange(e.target.value);
-      }}
-      value={input.value || value}
-    >
-      {options.map((option) => (
-        <FormControlLabel
-          key={option[valueKey || 'value'] || option}
-          value={option[valueKey || 'value'] || option}
-          control={<Radio />}
-          label={option[labelKey || 'label'] || option}
-        />
-      ))}
-    </RadioGroup>
-    {showError && (
-      <FormHelperText id="component-helper-text">{meta.error}</FormHelperText>
-    )}
-  </FormControl>
-);
+}) => {
+  const { colors } = React.useContext(ThemeContext);
+  return (
+    <FormControl component="fieldset">
+      <RadioGroup
+        aria-label="radio-group"
+        name={input.name || name || 'radio-group'}
+        row={Boolean(Row)}
+        onChange={(e) => {
+          onChange(e.target.value);
+          input.onChange(e.target.value);
+        }}
+        value={input.value || value}
+      >
+        {options.map((option) => (
+          <FormControlLabel
+            key={option[valueKey || 'value'] || option}
+            value={option[valueKey || 'value'] || option}
+            control={<Radio />}
+            label={option[labelKey || 'label'] || option}
+          />
+        ))}
+      </RadioGroup>
+      {showError && meta.touched && meta.error && (
+        <Text small color={colors.error} id="component-helper-text">
+          {meta.error}
+        </Text>
+      )}
+    </FormControl>
+  );
+};
 RadioInput.defaultProps = {
   onChange: () => {},
   input: {
